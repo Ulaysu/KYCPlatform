62195d8f7cab74d1a65a0df83777d27cd5f93911
Sulayman Sanyang <emiress921@gmail.com>
2026-01-31 19:46:58 +0000

    feat(BuidingBlock.Domain): create DomainException to enforce business rules and prevent invalid states

---
+++ BuildingBlocks.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Domain.Exceptions
+{
+    public abstract class DomainException : Exception
+    {
+        protected DomainException(string message)
+            : base(message)
+        {
+        }
+    }
+}
