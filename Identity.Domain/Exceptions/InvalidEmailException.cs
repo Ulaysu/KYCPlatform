d7dcc5e988d948096689105a3732802e8e1e85e0
Sulayman Sanyang <emiress921@gmail.com>
2026-01-31 20:36:37 +0000

    feat(Exceptions): created InvalidEmailExceptionn to handle erros related to emails

---
+++ Identity.Domain/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain.Exceptions
+{
+    public sealed class InvalidEmailException : DomainException
+    {
+        public InvalidEmailException(string message) : base(message)
+        {
+        }
+    }
+}
--- Identity.Domain/ValueObjects/Email.cs
+++ Identity.Domain/ValueObjects/Email.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using BuildingBlocks.Domain.Exceptions;
+using Identity.Domain.Exceptions;
 
 namespace Identity.Domain.ValueObjects
 {
