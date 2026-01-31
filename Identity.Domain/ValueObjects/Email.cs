509ca074b7fc3d62f1e4e2ff48bd4abef80cba18
Sulayman Sanyang <emiress921@gmail.com>
2026-01-31 20:26:46 +0000

    feat(valueObject): created email value object to define the rules that emails follow

---
+++ Identity.Domain/ValueObjects/Email.cs
@@ -0,0 +1,46 @@
+﻿using BuildingBlocks.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using BuildingBlocks.Domain.Exceptions;
+
+namespace Identity.Domain.ValueObjects
+{
+    public sealed class Email : ValueObject
+    {
+        public string Value { get; private set; }
+
+        public Email( string value) 
+        { 
+            Value = value;
+        }
+
+        public static Email Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new InvalidEmailException("Email cannot be empty.");
+
+            value = value.Trim().ToLowerInvariant();
+
+            if (!IsValid(value))
+                throw new InvalidEmailException("Invalid email format.");
+
+            return new Email(value);
+        }
+
+        private static bool IsValid(string email)
+        {
+            return Regex.IsMatch(
+                email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                RegexOptions.IgnoreCase
+            );
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
