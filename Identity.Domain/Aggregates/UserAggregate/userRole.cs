413889ec20b7ad0f6994a15b304a7c085fc06f31
Sulayman Sanyang <emiress921@gmail.com>
2026-02-01 20:10:34 +0000

    feat(Roles): Added user roles to control authorization

---
+++ Identity.Domain/Aggregates/UserAggregate/userRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain.Aggregates.UserAggregate
+{
+    public enum userRole
+    {
+        EmployerAdmin = 1,
+        EmployerHR = 2,
+        InternalAdmin = 3
+    }
+}
