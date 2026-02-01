9ae1d6f168721c90a30eb5b1c6bb650b8da5330f
Sulayman Sanyang <emiress921@gmail.com>
2026-02-01 19:59:19 +0000

    Update(baseEntity): Update base entity to have timestamps

--- BuildingBlocks.Domain/Common/BaseEntity.cs
+++ BuildingBlocks.Domain/Common/BaseEntity.cs
@@ -5,14 +5,23 @@ namespace BuildingBlocks.Domain.Common;
 public abstract class BaseEntity
 {
     public Guid Id { get; protected set; }
+
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; protected set; }
+
     private readonly List<IDomainEvent> _domainEvents = new();
     public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
 
     protected BaseEntity()
     {
         Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
     }
 
+    protected void MarKUpdated()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
     protected void AddDomainEvent(IDomainEvent domainEvent)
     {
         _domainEvents.Add(domainEvent);
