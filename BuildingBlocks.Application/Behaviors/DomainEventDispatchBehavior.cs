17b0b353d9da8fba4babea9176ff149cef0bd736
Sulayman Sanyang <emiress921@gmail.com>
2026-02-11 15:56:39 +0000

    feat(application): add DomainEventDispatchBehavior to dispatch domain events after commands

---
+++ BuildingBlocks.Application/Behaviors/DomainEventDispatchBehavior.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Behaviors
+{
+    public sealed class DomainEventDispatchBehavior
+    {
+    }
+}
