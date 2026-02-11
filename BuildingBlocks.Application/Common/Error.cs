5b985c3543bf701744ae1ad09d7d55dabf6b30cd
Sulayman Sanyang <emiress921@gmail.com>
2026-02-11 12:51:36 +0000

    feat(application): introduce Result pattern for standardized error handling

---
+++ BuildingBlocks.Application/Common/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Common
+{
+    public sealed record Error
+    (
+        string Code,
+        string Message,
+        ErrorType Type
+    );
+        
+    
+}
---
+++ BuildingBlocks.Application/Common/ErrorType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Common
+{
+    public enum ErrorType
+    {
+        Validation,
+        Conflict,
+        NotFound,
+        Unauthorized,
+        Forbidden,
+        Failure
+    }
+}
