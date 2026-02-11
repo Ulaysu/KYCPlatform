0546525923bad04ab6d91c9108c17a4892313d43
Sulayman Sanyang <emiress921@gmail.com>
2026-02-11 13:24:55 +0000

    feat(application): introduce IUnitOfWork abstraction for transaction consistency

---
+++ BuildingBlocks.Application/Abstractions/Persistence/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Abstractions.Persistence
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync(CancellationToken cancellationToken = default);
+    }
+}
