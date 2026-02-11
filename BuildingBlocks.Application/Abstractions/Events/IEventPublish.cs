151482e91d50ee85a0a59b1cfd40d962d4b3d8a8
Sulayman Sanyang <emiress921@gmail.com>
2026-02-11 13:18:41 +0000

    feat(application): add integration event abstractions for cross-service communication

---
+++ BuildingBlocks.Application/Abstractions/Events/IEventPublish.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Abstractions.Events
+{
+    public interface IEventPublish
+    {
+        Task PublishAsync(IIntegrationEvent integrationEvent, CancellationToken cancellationToken);
+    }
+}
---
+++ BuildingBlocks.Application/Abstractions/Events/IIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.Application.Abstractions.Events
+{
+    public interface IIntegrationEvent
+    {
+        Guid EventId { get; }
+        DateTimeOffset OccurredOn { get; }
+    }
+}
