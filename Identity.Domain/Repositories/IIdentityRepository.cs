befcc0db4f3447c033ce555145505617c884d151
Sulayman Sanyang <emiress921@gmail.com>
2026-02-14 02:18:27 +0000

    feat(Repository): createed Repository abstraction to be implemented in infastructure

---
+++ Identity.Domain/Repositories/IIdentityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Identity.Domain.Aggregates.UserAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Domain.Repositories
+{
+    public interface IIdentityRepository
+    {
+        Task<User?> GeByEmailAsync(string email, CancellationToken cancellationToken);
+        Task AddAsync(User user, CancellationToken cancellationToken);
+    }
+}
