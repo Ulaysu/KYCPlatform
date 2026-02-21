4a3e4ebb5c4e154b33bac948e614f9993cf82de8
Sulayman Sanyang <emiress921@gmail.com>
2026-02-21 01:55:56 +0000

    feat(command handler defining command handler for Identitiy.Application

---
+++ Identity.Application/Users/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Application.Abstractions.Messaging;
+using BuildingBlocks.Application.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Identity.Application.Users.RegisterUser
+{
+    public sealed record RegisterUserCommand
+    (
+        string FirstName,
+        string LastName,
+        string Email,
+        string Password
+
+
+    ) : ICommand<Result<Guid>>;
+    
+
+    
+}
---
+++ Identity.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using BuildingBlocks.Application.Abstractions.Events;
+using BuildingBlocks.Application.Abstractions.Messaging;
+using BuildingBlocks.Application.Abstractions.Persistence;
+using BuildingBlocks.Application.Common;
+using Identity.Domain.Aggregates.UserAggregate;
+using Identity.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Application.Users.RegisterUser
+{
+    public sealed class RegisterUserCommandHandler : ICommandHandler<RegisterUserCommand, Result<Guid>>
+    {
+        private readonly IIdentityRepository _repository;
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IEventPublish _eventPublisher;
+
+        public RegisterUserCommandHandler( 
+            IIdentityRepository repository, IUnitOfWork unitOfWork, IEventPublish eventPublisher)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+            _eventPublisher = eventPublisher;
+        }
+
+        public async Task<Result<Guid>> Handle(RegisterUserCommand command, CancellationToken cancellationToken)
+        {
+            // Check if user already exists
+            var existingUser = await _repository.GeByEmailAsync(command.Email, cancellationToken);
+            if (existingUser is not null) 
+            {
+                return Result<Guid>.Failure(
+                    new Error("User.Email.Exists", "User with this Email already exists", ErrorType.Conflict));
+            }
+
+            // Create Domain Entity
+            var user = User.Create(
+                command.FirstName);
+        }
+
+    }
+}
